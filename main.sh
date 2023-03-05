d9fb78fe5232d44fb7ed1a96c1a1124b293c673e
tarneaux <tarneo@tarneo.fr>
2023-03-05 12:07:11 +0100

    Correct bug in main.sh where there was only one option in dmenu displaying all scripts

--- main.sh
+++ main.sh
@@ -19,18 +19,7 @@ fi
 
 echo $dmscripts_dir
 
-# Choose a script
-scripts=$(find $dmscripts_dir/scripts/*.sh)
-
-# Remove the path from the script names
-scripts=$(echo $scripts | sed "s|$dmscripts_dir/scripts/||g")
-
-# Remove the .sh extension
-scripts=$(echo $scripts | sed "s|.sh||g")
-
-# Let the user choose a script
-chosen_one=$(echo $scripts | dmenu -i -p "Choose a script:")
-
+chosen=$(find $dmscripts_dir/scripts/ -type f | sed "s|$dmscripts_dir/scripts/||g" | sed "s|.sh||g" | dmenu)
 
 if [ -z "$chosen_one" ]; then
     exit 0
